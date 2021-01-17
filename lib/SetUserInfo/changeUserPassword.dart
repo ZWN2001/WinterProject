26f2dd426357b4065f8a360d576d19dc5fb6638b
Ryan Wong <718102754@qq.com>
2021-01-17 21:29:03 +0800

    顺丰快递

---
+++ lib/SetUserInfo/changeUserPassword.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+import 'setAccountInfo.dart';
+
+class ChangeUserPassword extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_rounded, color: Colors.white),
+            onPressed: () {
+              Navigator.push(context,MaterialPageRoute(builder: (context) => SetAccountInfo()));
+            },
+          ),
+          title: Text('修改密码'),
+        ),
+        body: ChangeUserPasswordPage(),
+      ),
+    );
+  }
+}
+
+class ChangeUserPasswordPage extends StatelessWidget {
+  final myTextController = TextEditingController();
+  final newPwdTextController1 = TextEditingController();
+  final newPwdTextController2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(20, 30, 20, 4),
+          child: TextField(
+            controller: myTextController,
+            decoration: (InputDecoration(labelText: '请输入原密码')),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(20, 30, 20, 4),
+          child: TextField(
+            controller: newPwdTextController1,
+            decoration: (InputDecoration(labelText: '请输入新密码')),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(20, 30, 20, 0),
+          child: TextField(
+            controller: newPwdTextController2,
+            decoration: (InputDecoration(labelText: '请确认新密码')),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(20, 50, 20, 0),
+            child: Center(
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  ),
+                  child: Text(
+                    'submit',
+                    style:
+                        TextStyle(fontSize: 25, color: Colors.lightBlueAccent),
+                  ),
+                  onPressed: () {
+                    if(newPwdTextController1.text==newPwdTextController2.text){
+
+
+                    }else{
+
+                    }
+                  },
+                ),
+              )
+        )
+      ],
+    );
+  }
+}
