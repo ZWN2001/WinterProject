4bbd8cdbc692bc41d52015be606bde3035e67edf
Ryan Wong <718102754@qq.com>
2021-01-21 15:29:27 +0800

    完善商品详细页面

--- lib/GoodsDetail/detailPage.dart
+++ lib/GoodsDetail/detailPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import './imageShowServer.dart';
@@ -30,6 +31,79 @@ class DetailPageState extends State<DetailPage> {
               scrollDirection: Axis.horizontal,
               onTap: (index){},
             ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(5, 10, 5, 0),
+            child: Text("商品简介(简单介绍有什么）,商品简介(简单介绍有什么）,商品简介(简单介绍有什么）【最多三行】",
+              maxLines: 3,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),),
+          ),
+          Container(
+            //height: 40,
+            padding: EdgeInsets.fromLTRB(5, 10, 5, 0),
+            child: Row(
+              children:<Widget> [
+                Expanded(
+                  flex: 1,
+                    child: Row(
+                      children: [
+                        Text("价格："),
+                        Text('￥100000000000000',style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 18
+                        ),)
+                      ],
+                    )),
+                /*Expanded(
+                  flex: 1,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children:<Widget> [
+                        Text("发布人："),
+                        ClipOval(
+                          child: Image.network(
+                              "https://www.itying.com/images/flutter/7.png",
+                            width: 20,
+                            height: 20,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Text("发布人的用户名")
+                      ],
+                    )),*/
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(5, 10, 5, 0),
+            child: Row(
+              children:<Widget> [
+                Expanded(
+                    child: Divider(
+                      height: 10,
+                      color: Colors.white,
+                      //indent: 120,
+                    )),
+                Expanded(child: Text("详细介绍",
+                  style: TextStyle(
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,)),
+                Expanded(
+                    child: Divider(
+                      height: 1,
+                      color: Colors.white,
+                      //indent: 120,
+                    ))
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(5, 10, 5, 0),
+            child: Text("详细介绍自己要卖什么，为什么卖，产品质量怎么样之类的"),
           )
         ],
       ),
