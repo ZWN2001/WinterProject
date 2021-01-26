0eb891a0291b40f769d67b1dd05ef98bb7f307be
Ryan Wong <718102754@qq.com>
2021-01-26 16:15:52 +0800

    添加聊天气泡组件，可直接使用

--- lib/ChatArea/ChatPage.dart
+++ lib/ChatArea/ChatPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:winter/AdapterAndHelper/DarkModeModel.dart';
 import 'package:provider/provider.dart';
+import 'package:winter/AdapterAndHelper/bubble.dart';
 
 class ChatPage extends StatefulWidget {
   @override
@@ -139,10 +140,21 @@ class ChatMessage extends StatelessWidget {
                       style: Theme.of(context).textTheme.subhead),
                       Container(
                         margin: const EdgeInsets.only(top: 5.0),
-                        child: Text(text,
+                        //margin: const EdgeInsets.fromLTRB(10, 5, 0, 0),
+                        child: Bubble(
+                          direction: BubbleDirection.left,
+                          color: Colors.blue,
+                          child: Text(
+                            text,
+                            style: TextStyle(
+                              color: DarkModeModel.darkMode ? Colors.white : Colors.black87
+                            ),
+                          ),
+                        )
+                        /*Text(text,
                           style: TextStyle(
                             color: DarkModeModel.darkMode ? Colors.white : Colors.black87
-                          ),),
+                          ),),*/
                       )
                     ],
                   ))
