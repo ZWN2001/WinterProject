48b0875b9fcf5293ae877295559ea9e519a40922
Ryan Wong <718102754@qq.com>
2021-02-10 20:50:46 +0800

    添加必要传参

--- lib/GoodsDetail/topNavigatorBar.dart
+++ lib/GoodsDetail/topNavigatorBar.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 import './detailPage.dart';
 
 class TopNavigatorBar extends StatefulWidget {
+  TopNavigatorBar({this.commodityId});
+  final int commodityId;
   @override
-  TopNavigatorBarState createState() => TopNavigatorBarState();
+  TopNavigatorBarState createState() => TopNavigatorBarState(commodityId: commodityId);
 }
 
 class TopNavigatorBarState extends State<TopNavigatorBar> {
+  TopNavigatorBarState({this.commodityId});
+  final int commodityId;
   @override
   Widget build(BuildContext context) {
    return Scaffold(
          appBar: AppBar(
            title: Text("详细页面"),
          ),
-         body: DetailPage(),
+         body: DetailPage(commodityId: commodityId,),
          );
   }
 
