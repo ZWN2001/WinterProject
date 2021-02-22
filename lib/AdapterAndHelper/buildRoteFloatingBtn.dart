6c9d33c209d942f55de56e056104f9d22223a130
Ryan Wong <718102754@qq.com>
2021-02-22 15:38:27 +0800

    没用的改动

--- lib/AdapterAndHelper/buildRoteFloatingBtn.dart
+++ lib/AdapterAndHelper/buildRoteFloatingBtn.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:shake_animation_widget/shake_animation_widget.dart';
 import 'package:toast/toast.dart';
 import 'package:winter/Basic/login.dart';
+import 'package:winter/AddGoodsAndNeeds/TabBarForAdd.dart';
+import 'package:winter/GoodsTypePage/allGoods.dart';
+
 class buildRoteFloatingBtn extends StatelessWidget{
+
+  int indexNum;
+
   Widget build(BuildContext context) {
   return  Positioned(
     right: 33,
@@ -15,10 +21,12 @@ class buildRoteFloatingBtn extends StatelessWidget{
         Icon(Icons.search),
       ],
       //点击事件回调
-      clickCallback: (int index){
+      clickCallback: (int index) async {
         if(index==0){
           if(LoginPageState.logged) {
-            Navigator.of(context).pushNamed('add');
+            indexNum = await Navigator.of(context).push(MaterialPageRoute(builder: (_) => TabBarForAdd()));
+            print('indexNum');
+            print(indexNum);
           }else{
             Toast.show("请先登录", context, duration: Toast.LENGTH_SHORT, gravity: Toast.BOTTOM);
             Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
